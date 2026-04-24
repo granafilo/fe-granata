f26680f5dcd6d66fb7c1297f9440754a744c5065
granafilo <granafilo@gmail.com>
2026-04-24 09:25:55 +0200

    Esercizio 2.3

--- js/esercizi/esercizi02.js
+++ js/esercizi/esercizi02.js
@@ -12,3 +12,10 @@ console.log(a*b);
 console.log(a/b);
 
 
+// Esercizio 2.3
+let prezzo = 49.90;
+const SCONTO = 20;
+let prezzoScontato = prezzo - (prezzo * SCONTO / 100);
+
+console.log(prezzoScontato);
+
