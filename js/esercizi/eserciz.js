148fa0b60750d545c08b6f9e79b7f8399c9c2cc7
granafilo <granafilo@gmail.com>
2026-04-24 12:48:03 +0200

    Esercizio 4.6

--- js/esercizi/eserciz.js
+++ js/esercizi/eserciz.js
@@ -1,7 +1,7 @@
 // Esercizio 4.1
 
 const BENVENUTO = (nome) => {
-    return `Benvenuto, ${nome}` 
+    return `Benvenuto, ${nome}`
 }
 
 console.log(BENVENUTO("FILIPPO"));
@@ -15,19 +15,32 @@ const rettangolo = (base, altezza) => {
 // Esercizio 4.3
 
 const celciusToFahenheit = (gradi) => {
-    return gradi * 9/5 + 32;
+    return gradi * 9 / 5 + 32;
 }
 
 // Esercizio 4.4
 
-const massimo = (a,b,c) => {
-    return Math.max(a,b,c);
+const massimo = (a, b, c) => {
+    return Math.max(a, b, c);
 }
 
-console.log(massimo(1,8,12));
+console.log(massimo(1, 8, 12));
 
 // Esercizio 4.5
 
 const isPari = (a) => {
     return a % 2 == 0 ? true : false;
 };
+
+// Esercizio 4.6
+
+const fattoriale = (a) => {
+    let fattoriale = 1
+    while (a > 0) {
+        fattoriale = a * fattoriale;
+        a--;
+    }
+    return fattoriale
+};
+
+console.log(fattoriale(6));
