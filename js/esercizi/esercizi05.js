a36768b0f1dee95b116492ee096220486bce9c59
granafilo <granafilo@gmail.com>
2026-04-27 11:43:35 +0200

    Esercizio 5.1

---
+++ js/esercizi/esercizi05.js
@@ -0,0 +1,115 @@
+// Esercizio 5.1
+console.log("Esercizio 5.1");
+
+const amici = ["Filippo", "Samuel", "Eric", "Arya", "Alessandro"]
+console.log(amici[0], amici.at(-1), amici.length);
+
+console.log("");
+
+// Esercizio 5.2
+console.log("Esercizio 5.2");
+
+console.log("");
+
+// Esercizio 5.3
+console.log("Esercizio 5.3");
+console.log("");
+// Esercizio 5.4
+console.log("Esercizio 5.4");
+console.log("");
+
+// Esercizio 5.5
+console.log("Esercizio 5.5");
+console.log("");
+
+// Esercizio 5.6
+console.log("Esercizio 5.6");
+console.log("");
+
+// Esercizio 5.7
+console.log("Esercizio 5.7");
+console.log("");
+
+// Esercizio 5.8
+console.log("Esercizio 5.8");
+console.log("");
+
+// Esercizio 5.9
+console.log("Esercizio 5.9");
+console.log("");
+
+// Esercizio 5.10
+console.log("Esercizio 5.10");
+console.log("");
+
+// Esercizio 5.11
+console.log("Esercizio 5.11");
+console.log("");
+
+// Esercizio 5.12
+console.log("Esercizio 5.12");
+console.log("");
+
+// Esercizio 5.13
+console.log("Esercizio 5.13");
+console.log("");
+
+// Esercizio 5.14
+console.log("Esercizio 5.14");
+console.log("");
+
+// Esercizio 5.15
+console.log("Esercizio 5.15");
+console.log("");
+
+// Esercizio 5.16
+console.log("Esercizio 5.16");
+console.log("");
+
+// Esercizio 5.17
+console.log("Esercizio 5.17");
+console.log("");
+
+// Esercizio 5.18
+console.log("Esercizio 5.18");
+console.log("");
+
+// Esercizio 5.19
+console.log("Esercizio 5.19");
+console.log("");
+
+// Esercizio 5.20
+console.log("Esercizio 5.20");
+console.log("");
+
+// Esercizio 5.21
+console.log("Esercizio 5.21");
+console.log("");
+
+// Esercizio 5.22
+console.log("Esercizio 5.22");
+console.log("");
+
+// Esercizio 5.23
+console.log("Esercizio 5.23");
+console.log("");
+
+// Esercizio 5.24
+console.log("Esercizio 5.24");
+console.log("");
+
+// Esercizio 5.25
+console.log("Esercizio 5.25");
+console.log("");
+
+// Esercizio 5.26
+console.log("Esercizio 5.26");
+console.log("");
+
+// Esercizio 5.27
+console.log("Esercizio 5.27");
+console.log("");
+
+// Esercizio 5.28
+console.log("Esercizio 5.28");
+console.log("");
